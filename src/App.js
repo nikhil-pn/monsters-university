42ece782923bf198399c72c139f8a3b8964870af
nikhilpn725 <nikhilpn725@gmail.com>
2022-11-11 12:49:19 +0530

    add src

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ReactDOM, Component } from "react";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      monster : []
+    }
+  }
+  componentDidMount(){
+    const respones = fetch("https://jsonplaceholder.typicode.com/users").then(respones=> respones.json())
+    .then(user => this.setState({monster : user}))
+  }
+
+  render(){
+    return(
+      <div className="App">
+      
+        {this.state.monster.map(user => <h1 key={user.id}> {user.name}</h1>)} 
+
+       <button onClick={()=>this.setState({monster: [{name : "nikhil", id: "3"}, {name : "soniya", id: "4"}]})}>Update</button>
+      </div>
+
+    )
+  }
 }
-
 export default App;
