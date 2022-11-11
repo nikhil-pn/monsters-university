a56b7056b1e95bd88c241560fb4e4410288c441c
nikhilpn725 <nikhilpn725@gmail.com>
2022-11-11 18:34:49 +0530

    adding

--- src/App.js
+++ src/App.js
@@ -2,23 +2,40 @@ import React from "react";
 import { ReactDOM, Component } from "react";
 import "./App.css"
 import { CardLists } from "./components/classLists/cardLists.component";
+import {SearchBox} from "./components/searchBox/search.component"
 
 class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      monster : []
+      monsters : [],
+      searchField : ""
     }
   }
   componentDidMount(){
     const respones = fetch("https://jsonplaceholder.typicode.com/users").then(respones=> respones.json())
-    .then(user => this.setState({monster : user}))
+    .then(user => this.setState({monsters : user}))
+  }
+
+  handleChange= (e)=>{
+    this.setState({searchField : e.target.value})
   }
 
   render(){
+
+   
+    const {monsters, searchField} = this.state;
+    //filters the search for monsters uses the filter function
+    const filteredMonster = monsters.filter(monster=>monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return(
       <div className="App">
-        <CardLists monster={this.state.monster} >
+        <h1 className="title-font">Monsters University</h1>
+     
+        <SearchBox
+        placeholder="Search Monsters" 
+        handleChange = {this.handleChange}/>
+
+        <CardLists monster={filteredMonster} >
        
         </CardLists>
        <button onClick={()=>this.setState({monster: [{name : "nikhil", id: "3"}, {name : "soniya", id: "4"}]})}>Update</button>
